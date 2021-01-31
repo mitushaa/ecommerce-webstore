5b77669d3f9cac6cb9040d1015411a6712dbb9f1
mitushaa <aryamitusha@gmail.com>
2021-01-31 13:09:27 +0530

    Adding Products, Product

---
+++ ecommerce-webstore/src/components/Products/Product/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/cores'
+
+const Product = () => {
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant= "h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant= "h5">
+                        {product.name}
+                    </Typography>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Product
